5363e56e6057a2ff2fe8af735ef9d5f85f0e4465
Ngo Nam <namngo102003@gmail.com>
2024-05-19 00:39:27 +0700

    config hardhat

--- NFTContract/hardhat.config.ts
+++ NFTContract/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/27751eae5f47427689aa0d340be58945',
+      accounts: [process.env.PRIVATE_KEY ?? '']
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY ?? '',
+    }
+  }
+
 };
 
 export default config;
