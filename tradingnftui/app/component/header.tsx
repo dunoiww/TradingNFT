0da3cd688c620dfd4aa1e58337a7ba1f7d91e6f9
Ngo Nam <namngo102003@gmail.com>
2024-06-01 00:35:38 +0700

    Header component, change title

--- tradingnftui/app/component/header.tsx
+++ tradingnftui/app/component/header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
     };
 
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b sticky border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link
                     href="market/"
