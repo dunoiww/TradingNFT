aca584390bbf831ae84a27785b059884cdc04a2b
Ngo Nam <namngo102003@gmail.com>
2024-06-02 13:48:07 +0700

    Processing modal

---
+++ tradingnftui/app/component/ProcessingModal.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Modal, Box, Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import { useAppSelector } from '@/app/redux/hooks';
+import NftContract from '@/app/contracts/NftContract';
+import { showTransactionHash } from '../utils';
+import { PulseLoader } from 'react-spinners';
+
+interface ProcessingModalProps {
+    isLoading: boolean;
+}
+
+export default function ProcessingModal({ isLoading }: ProcessingModalProps) {
+    return (
+        <div>
+            <Modal open={isLoading}>
+                <div className="flex flex-col items-center justify-center w-full h-full">
+                    <div
+                        className="loader border-t-2 rounded-full border-yellow-500 bg-yellow-300 animate-spin aspect-square w-12 flex justify-center items-center text-yellow-700">
+                        <p className='text-2xl'>€</p>
+                    </div>
+                    <div className='flex items-end justify-center'>
+                        <p className='mt-5'>Wait the transaction for a moment </p>
+                        <PulseLoader color="#fde046" size={10} margin={4}/> 
+                    </div>
+                    
+                </div>
+            </Modal>
+        </div>
+    );
+}
