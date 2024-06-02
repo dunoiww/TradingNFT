a19bf7fe097a91d83a4b218e3b7d8acfb6c83072
Ngo Nam <namngo102003@gmail.com>
2024-06-02 14:51:48 +0700

    update something

--- tradingnftui/app/_types_/index.ts
+++ tradingnftui/app/_types_/index.ts
@@ -8,13 +8,30 @@ export interface IMenu {
     url: string;
 }
 
-export type ActionType = "LIST" | "UNLIST" | "TRANSFER"
+export type ActionType = 'LIST' | 'UNLIST' | 'TRANSFER';
+
+export interface IAttribute {
+    trait_type: string;
+    value: string | number;
+}
 
 export interface INftItem {
     id: number;
     name?: string;
     description?: string;
     image: string;
+    attributes?: IAttribute[];
     price?: number;
     author?: string;
+}
+
+export enum Clarity {
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'S',
+    'SS',
+    'SSS',
 }
---
+++ tradingnftui/app/constants/index.ts
@@ -0,0 +1,6 @@
+import { IMenu } from "../_types_";
+
+export const menus: IMenu[] = [
+    {name: "Market", url: '/market'},
+    {name: "Profile", url: '/profile'}
+]
