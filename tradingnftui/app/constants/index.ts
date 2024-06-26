6fc0b6242beac30722524f28b00e49617f963f84
Ngo Nam <namngo102003@gmail.com>
2024-06-26 17:02:46 +0700

    Update something

--- tradingnftui/app/constants/index.ts
+++ tradingnftui/app/constants/index.ts
@@ -2,5 +2,6 @@ import { IMenu } from "../_types_";
 
 export const menus: IMenu[] = [
     {name: "Market", url: '/market'},
-    {name: "Profile", url: '/profile'}
+    {name: "Profile", url: '/profile'},
+    {name: "Setting", url: '/setting'}
 ]
